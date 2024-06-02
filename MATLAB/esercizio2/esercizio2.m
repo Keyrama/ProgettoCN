ea1864cd651bb39e38420e7afc74b9a503b01136
Claudio Soricaro <127410758+Keyrama@users.noreply.github.com>
2024-06-02 22:43:39 +0200

    Delete MATLAB directory

--- MATLAB/esercizio2/esercizio2.m
+++
@@ -1,8 +0,0 @@
-x = linspace(1, 5/3, 100001);
-f = @(x) 1 + x.^2 + (log(abs( 3*(1 - x) + 1)))/80;
-xmin = fminbnd(f,1,5/3);
-disp(['Il minimo della funzione è in x = ', num2str(xmin)]);
-disp(['Il valore minimo della funzione è f(x) = ', num2str(f(xmin))]);
-plot(x,f(x));
-
-
--- MATLAB/esercizio28/Esercizio28.m
+++
@@ -1,7 +0,0 @@
-for i=1:7
-    c = pesiNewtonCotes(i);
-    rational = rats(c);
-    disp(rational);
-end
-
-disp(rats(pesiNewtonCotes(9)));
